b7448091b5b4b36b667b32a1cc7a23e868618edf
Ian Souza <ianfmsouza@gmail.com>
2024-10-11 00:07:02 -0300

    chore: adjust DatabaseSeeder and generate a new migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,6 +15,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         User::factory()->count(200)->create();
-        Project::factory()->count(10)->create();
+        User::query()->inRandomOrder()->limit(10)->get()->each(fn(User $u)=> Project::factory()->create(['creadted_by' => $u->id]));   
     }
 }
